e7fd39553cbfb304ba09ea6aace7600c0affe5c3
Márcio Júnior <marcio.mj70@gmail.com>
2021-07-19 16:06:09 -0300

    episode 1 to 4

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">
+        Gerador de nomes utilizando Vue.js, GraphQL e Node
+      </h6>
+    </div>
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    DomainList,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+@media (max-width: 767.5px) {
+  #sufix {
+    margin-top: 20px;
+  }
 }
 </style>
